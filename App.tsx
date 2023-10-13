6b8fa9c6fc40580662475f8b0ac77d796b07e49a
Stefano Cutri <stefanocutristecco@gmail.com>
2023-10-13 17:05:32 -0300

    Move HomeScreen in separated layer

--- App.tsx
+++ App.tsx
@@ -1,17 +1,7 @@
-// In App.js in a new project
-
 import * as React from 'react';
-import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+import HomeScreen from './src/screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
