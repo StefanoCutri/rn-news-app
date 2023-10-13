f66018bf78fce4dccd632611bce946dd2c7b72fc
Stefano Cutri <stefanocutristecco@gmail.com>
2023-10-13 18:14:13 -0300

    Add newApi interface

---
+++ src/interfaces/index.ts
@@ -0,0 +1,22 @@
+
+export interface NewsAPI {
+    status:       string;
+    totalResults: number;
+    articles:     Article[];
+}
+
+export interface Article {
+    source:      Source;
+    author:      null | string;
+    title:       string;
+    description: string;
+    url:         string;
+    urlToImage:  string;
+    publishedAt: string;
+    content:     null | string;
+}
+
+export interface Source {
+    id:   null | string;
+    name: string;
+}
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,12 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import { View, Text } from 'react-native'
+import React from 'react'
 
 const HomeScreen = () => {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
     </View>
-  );
-};
+  )
+}
 
-export default HomeScreen;
+export default HomeScreen
