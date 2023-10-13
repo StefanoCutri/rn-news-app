42cdbf2924e17098df0560a18e1b30ba4546bff1
Stefano Cutri <stefanocutristecco@gmail.com>
2023-10-13 19:53:28 -0300

    Remove header

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/HomeScreen';
-import { Provider } from 'react-redux';
-import { store } from './src/context/store';
+import {Provider} from 'react-redux';
+import {store} from './src/context/store';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <Provider store={store}>
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 }
