b0705f2e6024f4f9cdcfa4cb229d71002c9312db
Stefano Cutri <stefanocutristecco@gmail.com>
2023-10-16 18:39:00 -0300

    Add bottom tab navigation

--- App.tsx
+++ App.tsx
@@ -4,9 +4,21 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {Provider} from 'react-redux';
 import {store} from './src/features/store';
 import HomeScreen from './src/screens/HomeScreen';
-import ArticleScreen from './src/screens/ArticleScreen';
-import Header from './src/components/ui/Header/Header';
 import FavouritesScreen from './src/screens/FavouritesScreen';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {HomeIcon, HeartIcon} from 'react-native-heroicons/outline';
+import ArticleScreen from './src/screens/ArticleScreen';
+
+const Tab = createBottomTabNavigator();
+
+function HomeStack() {
+  return (
+    <Stack.Navigator>
+    <Stack.Screen name="Home" component={HomeScreen} />
+    <Stack.Screen name="Article" component={ArticleScreen} />
+  </Stack.Navigator>
+  );
+}
 
 const Stack = createNativeStackNavigator();
 
@@ -14,15 +26,27 @@ function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Stack.Navigator
-        screenOptions={{ headerTitle: (props) => <Header /> }}
-        >
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Article" component={ArticleScreen} />
-          <Stack.Screen name="Favourites" component={FavouritesScreen} options={{
-            headerShown: false
-          }}/>
-        </Stack.Navigator>
+        <Tab.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}>
+          <Tab.Screen
+            name="HomeTab"
+            component={HomeStack}
+            options={{
+              tabBarActiveTintColor: '#000',
+              tabBarIcon: () => <HomeIcon color="#363062" />,
+            }}
+          />
+          <Tab.Screen
+            name="Favourites"
+            component={FavouritesScreen}
+            options={{
+              tabBarActiveTintColor: '#000',
+              tabBarIcon: () => <HeartIcon color="#363062" />,
+            }}
+          />
+        </Tab.Navigator>
       </NavigationContainer>
     </Provider>
   );
